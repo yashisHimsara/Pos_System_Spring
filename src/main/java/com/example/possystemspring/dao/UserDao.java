e61e4d07722f5c60bb32d298d7042ae993d87cdf
yashisHimsara <139766123+yashisHimsara@users.noreply.github.com>
2024-10-07 20:59:13 +0530

    Update UserDao.java

--- src/main/java/com/example/possystemspring/dao/UserDao.java
+++ src/main/java/com/example/possystemspring/dao/UserDao.java
@@ -4,7 +4,6 @@ import com.example.notecollector.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
 public interface UserDao extends JpaRepository<User,String> {
 }
